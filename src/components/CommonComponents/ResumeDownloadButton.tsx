68c66573a7e08d0367815118fb14b7cc0b34f73a
Rahul Mallik <rmallik191242@bscse.uiu.ac.bd>
2026-07-05 13:15:15 +0600

    fix: Correct case in resume download filename in ResumeDownloadButton component

--- src/components/CommonComponents/ResumeDownloadButton.tsx
+++ src/components/CommonComponents/ResumeDownloadButton.tsx
@@ -4,7 +4,7 @@ import { ArrowDownToLine } from 'lucide-react'
 
 const ResumeDownloadButton = () => {
   return (
-    <a href="/resume_of_rahul_mallik.pdf" download="resume_of_rahul_mallik.pdf" className="inline-block">
+    <a href="/Resume_of_Rahul_Mallik.pdf" download="Resume_of_Rahul_Mallik.pdf" className="inline-block">
                 <Button  className="group inline-flex  items-center justify-center gap-2 px-6 xl:px-7 py-3 xl:py-3.5 rounded-lg border-[0.5px] border-solid border-[#95EDFF] bg-[linear-gradient(134deg,rgba(37,37,37,1)_17%,rgba(132,132,132,1)_100%)] hover:opacity-90 transition-opacity">
                   <span className="font-normal text-[#95EDFF] text-base xl:text-lg text-center leading-[21.6px] whitespace-nowrap tracking-[0]">
                     View my Resume
